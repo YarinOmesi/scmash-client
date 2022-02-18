a75fdebe33148c79646bf5e103d0019bbb2c9295
Yarin Omesi <yarinomesi@gmail.com>
2022-02-18 21:54:03 +0200

    Designed feedback-view

--- src/comonnents/feedback/feedback.tsx
+++ src/comonnents/feedback/feedback.tsx
@@ -1,17 +1,36 @@
+import {Reaction} from "../../Reaction";
+import {Card, CardActions, CardContent, IconButton, Typography} from "@mui/material";
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'
+import ThumbDownIcon from '@mui/icons-material/ThumbDown'
 
 export type FeedbackProp = {
-    content:string,
-    writerNickname:string,
-    subject:string
+    content: string,
+    writerNickname: string,
+    subject: string,
+    currentReaction: (Reaction | null)
+    onReactionClicked: (reaction: Reaction) => void
 }
 
-export function Feedback(props:FeedbackProp){
 
+export function Feedback(props: FeedbackProp) {
+    function getColor(reaction:Reaction): ('primary' | 'default'){
+        return props.currentReaction === reaction ? 'primary' :'default'
+    }
     return (
-        <pre>
-            Subject:{props.subject}
-            Content: {props.content}
-            WriterNickName:{props.writerNickname}
-        </pre>
+        <Card sx={{display: 'inline-block', minWidth: 200}}>
+            <CardContent>
+                <Typography variant="h5" component="div">{props.subject}</Typography>
+                <Typography sx={{mb: 1.5}} color="text.secondary">{props.writerNickname}</Typography>
+                <Typography variant="body2">{props.content}</Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton onClick={() => props.onReactionClicked(Reaction.Like)} color={getColor(Reaction.Like)} >
+                    <ThumbUpAltIcon/>
+                </IconButton>
+                <IconButton onClick={() =>  props.onReactionClicked(Reaction.Dislike)} color={getColor(Reaction.Dislike)}>
+                    <ThumbDownIcon/>
+                </IconButton>
+            </CardActions>
+        </Card>
     )
 }
