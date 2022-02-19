c030baaac0fef1df4ad4dbeb61fd353ca12f4e11
Yarin Omesi <yarinomesi@gmail.com>
2022-02-20 00:35:42 +0200

    Made id be string or undefined

--- src/models/FeedbackModel.ts
+++ src/models/FeedbackModel.ts
@@ -1,18 +1,19 @@
 import {Reaction} from "./Reaction";
 
 export class FeedbackModel {
-    id: string;
+    id: (string|undefined);
     content: string;
     writerNickname: string;
     subject: string;
     reaction: Reaction;
 
-    constructor(id: string,subject: string, content: string, writerNickname: string) {
+    constructor(id:(string|undefined),subject: string, content: string, writerNickname: string) {
         this.id = id;
         this.content = content;
         this.writerNickname = writerNickname;
         this.subject = subject;
         this.reaction = Reaction.NoReaction;
     }
+
 
 }
