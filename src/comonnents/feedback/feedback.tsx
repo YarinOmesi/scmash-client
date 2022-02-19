ddc343d1ff99e10982b3c0e2a51d15f3741b8d51
Yarin Omesi <yarinomesi@gmail.com>
2022-02-19 17:00:54 +0200

    Now Used FeedbackModel instead of FeedbackData

--- src/comonnents/feedback/feedback.tsx
+++ src/comonnents/feedback/feedback.tsx
@@ -2,32 +2,34 @@ import {Reaction} from "../../Reaction";
 import {Card, CardActions, CardContent, IconButton, Typography} from "@mui/material";
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'
 import ThumbDownIcon from '@mui/icons-material/ThumbDown'
+import {FeedbackModel} from "./FeedbackModel";
 
 export type FeedbackProp = {
-    content: string,
-    writerNickname: string,
-    subject: string,
-    currentReaction: (Reaction | null)
+    feedbackModel: FeedbackModel
     onReactionClicked: (reaction: Reaction) => void
 }
 
 
 export function Feedback(props: FeedbackProp) {
-    function getColor(reaction:Reaction): ('primary' | 'default'){
-        return props.currentReaction === reaction ? 'primary' :'default'
+    const {feedbackModel, onReactionClicked} = props
+
+    function getColor(reaction: Reaction): ('primary' | 'default') {
+        return feedbackModel.reaction === reaction ? 'primary' : 'default'
     }
+
     return (
-        <Card sx={{display: 'inline-block', minWidth: 200}}>
+        <Card sx={{display: 'inline-block', minWidth: 200}} key={feedbackModel.id}>
             <CardContent>
-                <Typography variant="h5" component="div">{props.subject}</Typography>
-                <Typography sx={{mb: 1.5}} color="text.secondary">{props.writerNickname}</Typography>
-                <Typography variant="body2">{props.content}</Typography>
+                <Typography variant="h5" component="div">{feedbackModel.subject}</Typography>
+                <Typography sx={{mb: 1.5}} color="text.secondary">{feedbackModel.writerNickname}</Typography>
+                <Typography variant="body2">{feedbackModel.content}</Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton onClick={() => props.onReactionClicked(Reaction.Like)} color={getColor(Reaction.Like)} >
+                <IconButton onClick={() => onReactionClicked(Reaction.Like)} color={getColor(Reaction.Like)}>
                     <ThumbUpAltIcon/>
                 </IconButton>
-                <IconButton onClick={() =>  props.onReactionClicked(Reaction.Dislike)} color={getColor(Reaction.Dislike)}>
+                <IconButton onClick={() => onReactionClicked(Reaction.Dislike)}
+                            color={getColor(Reaction.Dislike)}>
                     <ThumbDownIcon/>
                 </IconButton>
             </CardActions>
