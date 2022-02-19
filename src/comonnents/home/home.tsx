cb9f50263d01c301e09da705964126dbb70e8dc9
Yarin Omesi <yarinomesi@gmail.com>
2022-02-19 17:15:07 +0200

    Clean Home file

--- src/comonnents/home/home.tsx
+++ src/comonnents/home/home.tsx
@@ -1,120 +1,52 @@
-import {
-    AppBar,
-    Box, Button, Card, CardActions, CardContent,
-    Tab,
-    Tabs,
-    TextField,
-    Toolbar,
-    Typography
-} from "@mui/material";
-import React, {useState} from "react"
-import {FeedbackData, FeedbackGrid} from "./feedbackGrid";
+import {Box} from "@mui/material";
+import React, {useEffect, useState} from "react"
+import {Feedbacks} from "../../feedbacks";
+import {FeedbackModel} from "../feedback/FeedbackModel";
+import {MyAppBar} from "./myAppBar";
+import {FeedbackInput} from "./feedbackInput";
+import {SubjectsTabs} from "./subjectsTabs";
 
 
-type Subject = {
+export type Subject = {
     name: string,
-    feedbacks: Array<FeedbackData>
+    feedbacks: Array<FeedbackModel>
 }
 
-function MyAppBar() {
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" color="inherit" component="div">
-                    Auto Scmash
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    )
-}
-
-function TabPanel(props: { getFeedbacks: () => Array<FeedbackData>, value: number, index: number }) {
-
-    return (
-        <Box>
-            {props.value === props.index &&
-                <FeedbackGrid feedbacks={props.getFeedbacks()}/>
-            }
-        </Box>
-    )
-}
-
-function MyTabs(props: { tabIndex: number, setValue: (tabIndex: number) => void, subjects: Array<Subject> }) {
-    const {tabIndex, setValue, subjects} = props
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-    };
-    return (
-        <Box>
-            <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-                <Tabs value={tabIndex} onChange={handleChange} aria-label="basic tabs example">
-                    {
-                        subjects.map(subject => (<Tab label={subject.name}/>))
-                    }
-                </Tabs>
-            </Box>
-            {
-                props.subjects.map((subject, index) => {
-                    return <TabPanel value={tabIndex} index={index} getFeedbacks={() => subject.feedbacks}/>
-                })
-            }
-        </Box>
-    )
-}
-
-
-function FeedbackInput(props: { feedback: string, setFeedback: (value: string) => void, onSend: () => void }) {
-    const {feedback, setFeedback, onSend} = props
-    return (
-        <Card>
-            <CardContent>
-                <TextField
-                    value={feedback}
-                    label="Write a feedback..."
-                    variant="standard"
-                    onChange={(e) => setFeedback(e.target.value)}
-                />
-            </CardContent>
-            <CardActions>
-                <Button color="primary" onClick={() => onSend()}>
-                    Send
-                </Button>
-
-            </CardActions>
-        </Card>
-    )
+function sendFeedback(subject: Subject, content: string) {
+    Feedbacks.SendFeedback({subject: subject.name, content: content})
 }
 
 export function Home() {
     const [tabIndex, setTabIndex] = useState<number>(0)
     const [feedback, setFeedback] = useState<string>("")
-    const [subjects, setSubjects] = useState<Array<Subject>>([
-        {name: "logistics", feedbacks: []},
-        {name: "site", feedbacks: []},
-    ])
+    const [subjects, setSubjects] = useState<Array<Subject>>([])
+
+    useEffect(() => {
+        const listener = (newSubjects: Array<Subject>) => setSubjects(newSubjects)
+        Feedbacks.OnSubjectsChanged(listener)
+        return function cleanEffect() {
+            Feedbacks.RemoveListener(listener)
+        }
+    }, [])
+
+    function onFeedbackSend() {
+        sendFeedback(subjects[tabIndex], feedback)
+        setFeedback("")
+    }
+
     return (
         <Box sx={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
             <MyAppBar/>
 
             <Box sx={{flexGrow: 1}}>
-                <MyTabs tabIndex={tabIndex} setValue={(i: number) => setTabIndex(i)} subjects={subjects}/>
+                <SubjectsTabs tabIndex={tabIndex} setValue={(i: number) => setTabIndex(i)} subjects={subjects}/>
             </Box>
 
-            <FeedbackInput setFeedback={(value:string) => setFeedback(value)} feedback={feedback} onSend={() => {
-                // handle feedback send
-                const subject: Subject = subjects[tabIndex]
-                const newFeedback: FeedbackData = {
-                    subject: subject.name,
-                    content: feedback,
-                    writerNickname: 'nigger'
-                }
-                const newFeedBacks = subjects[tabIndex].feedbacks.concat([newFeedback])
-                let newSubjects = subjects.slice()
-                newSubjects[tabIndex] = {...subject, feedbacks: newFeedBacks}
-                setSubjects(newSubjects)
-                setFeedback("")
-            }}/>
-
+            <FeedbackInput
+                setFeedback={(value: string) => setFeedback(value)}
+                feedback={feedback}
+                onSend={() => onFeedbackSend()}
+            />
         </Box>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
+import { Home} from './comonnents/home/home'
 
 ReactDOM.render(
   <React.StrictMode>
-
+    <Home  />
   </React.StrictMode>,
   document.getElementById('root')
 );
