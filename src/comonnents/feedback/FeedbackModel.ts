cbece691860278b80ff34573ce167a72cd086d23
Yarin Omesi <yarinomesi@gmail.com>
2022-02-19 16:59:35 +0200

    Added FeedbackModel

---
+++ src/comonnents/feedback/FeedbackModel.ts
@@ -0,0 +1,18 @@
+import {Reaction} from "../../Reaction";
+
+export class FeedbackModel {
+    id: string;
+    content: string;
+    writerNickname: string;
+    subject: string;
+    reaction: Reaction;
+
+    constructor(id: string,subject: string, content: string, writerNickname: string) {
+        this.id = id;
+        this.content = content;
+        this.writerNickname = writerNickname;
+        this.subject = subject;
+        this.reaction = Reaction.NoReaction;
+    }
+
+}
